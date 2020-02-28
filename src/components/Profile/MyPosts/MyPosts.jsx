5c2b75e79b6b71dc5743b1d3661c7eb3ec37ddcd
Olesea Ciumac <emmy.gold@gmail.com>
2020-02-28 12:15:25 +0000

    PureComponent example on MyPosts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import Post from "./Post/Post";
-import css from "./MyPosts.module.scss"
+import css from "./MyPosts.module.scss";
 
-function MyPosts(props) {
-
-    let posts = props.posts.map( (p) => {
-        return(<Post key={p.id} photo={p.photo} text={p.text} likes={p.likes} />);
-    } );
-
-    return(
-        <section className={css.my_posts}>
-            <h3>My posts</h3>
-            {posts}
-        </section>
-    );
+class MyPosts extends PureComponent {
+    
+    render() {
+        let posts = this.props.posts.map( (p) => {
+            return(<Post key={p.id} photo={p.photo} text={p.text} likes={p.likes} />);
+        } );
+        return(
+            <section className={css.my_posts}>
+                <h3>My posts</h3>
+                {posts}
+            </section>
+        );
+    }
 }
 
 export default MyPosts;
