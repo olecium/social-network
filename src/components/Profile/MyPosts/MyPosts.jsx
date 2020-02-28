e4962102b633aa60e0bc9f55f9cc750fa11fd0e0
Olesea Ciumac <emmy.gold@gmail.com>
2020-02-28 12:22:15 +0000

    Using React.memo instead of PureComponent and ShouldComponentUpdate

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,20 +1,20 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import Post from "./Post/Post";
 import css from "./MyPosts.module.scss";
 
-class MyPosts extends PureComponent {
+const MyPosts = React.memo(props => {
     
-    render() {
-        let posts = this.props.posts.map( (p) => {
-            return(<Post key={p.id} photo={p.photo} text={p.text} likes={p.likes} />);
-        } );
-        return(
-            <section className={css.my_posts}>
-                <h3>My posts</h3>
-                {posts}
-            </section>
-        );
-    }
-}
+    let posts = props.posts.map( (p) => {
+        return(<Post key={p.id} photo={p.photo} text={p.text} likes={p.likes} />);
+    } );
+
+    return(
+        <section className={css.my_posts}>
+            <h3>My posts</h3>
+            {posts}
+        </section>
+    );
+
+});
 
 export default MyPosts;
