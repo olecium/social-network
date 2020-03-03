004c68ae6b1b4b289a677cd81cc6c83924e79ff6
Olesea Ciumac <emmy.gold@gmail.com>
2020-03-03 10:36:30 +0000

    Paginator updated. Implemented lazy loading for Content.

--- src/components/Main/Content/Content.jsx
+++ src/components/Main/Content/Content.jsx
@@ -1,24 +1,32 @@
-import React from "react";
-import {Route, Redirect} from "react-router-dom";
-import ProfileContainer from "../../Profile/ProfileContainer";
-import News from "../../News/News";
-import Music from "../../Music/Music";
-import Settings from "../../Settings/Settings";
+import React, { Suspense } from "react";
+import {Route, Redirect, Switch} from "react-router-dom";
 import css from "./Content.module.scss";
-import MessagesContainer from "../../Messages/MessagesContainer";
-import UsersContainer from "../../Users/UsersContainer";
+
+import ProfileContainer from "../../Profile/ProfileContainer";
 import LoginContainer from "../../Login/LoginContainer";
+import Preloader from "../../common/Preloader/Preloader";
+import { withSuspense } from "../../../hoc/withSuspense";
+
+const MessagesContainer = React.lazy(() => import('./../../Messages/MessagesContainer'));
+const UsersContainer = React.lazy(() => import('./../../Users/UsersContainer'));
+const News = React.lazy(() => import('./../../News/News'));
+const Music = React.lazy(() => import('./../../Music/Music'));
+const Settings = React.lazy(() => import('./../../Settings/Settings'));
 
 const Content = () => {
     return(
         <section className={css.app_content}>
             <Route exact path='/' render={() => <Redirect to={"/profile"}/>}/>
-            <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
-            <Route path="/messages" render={() => <MessagesContainer />} />
-            <Route path="/users" render={() => <UsersContainer />} />
-            <Route path="/news" component={News} />
-            <Route path="/music" component={Music} />
-            <Route path="/settings" component={Settings} />
+            <Route path="/profile/:userId?" render={ withSuspense(ProfileContainer) }/>
+            <Route path="/messages" render={ withSuspense(MessagesContainer) }/>
+            <Route path="/users" render={ withSuspense(UsersContainer) }/>
+            <Suspense fallback={<Preloader />}>
+                <Switch>
+                    <Route path="/news" component={News} />
+                    <Route path="/music" component={Music} />
+                    <Route path="/settings" component={Settings} />
+                </Switch>
+            </Suspense>
             <Route path="/login" component={LoginContainer} />
         </section>
     );
--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import css from './Paginator.module.scss';
 
-const Paginator = ({currentPage, totalItemsCount, onPageChange, pageSize}) => {
+const Paginator = ({currentPage, totalItemsCount, onPageChange, pageSize, portionSize = 10}) => {
 
     let pagesCount = Math.ceil(totalItemsCount / pageSize);
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) pages.push(i);
     
+    let portionCount = Math.ceil(pagesCount / portionSize);
+    let [portionCounter, setPortionNumber] = useState(1);
+    let leftPortionPageNumber = (portionCounter - 1) * portionSize + 1;
+    let rightPortionPageNumber = portionCounter * portionSize;
+
     return(
         <ul className={css.pagination}>
             {
-                pages.map( p => {
+                portionCounter > 1 &&
+                <li><button onClick={() => { setPortionNumber(portionCounter - 1) }}>&lt;</button></li>
+            }
+            {
+                pages
+                .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+                .map( p => {
                     return <li key={p} className={currentPage === p ? `${css.page} ${css.page__selected}` : css.page}>
                         <span onClick={() => {onPageChange(p)}}>{p}</span>
                     </li>
                 })
+            }
+            {   
+                portionCounter < portionCount &&
+                <li><button onClick={() => { setPortionNumber(portionCounter + 1) }}>&gt;</button></li>
             }
             
         </ul>
